ccd2b554015e200c7c60d50beb9f0fe97f01b1d5
Bradley <gonzales.bradleyd@gmail.com>
2022-06-30 17:03:33 -0400

    Added simple UI

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-let res = "";
-let userPoints = 0;
+//GAME LOGIC
+let res;
 let computerPoints = 0;
+let userPoints = 0;
+let winnerText;
+function disableButtons() {
+    for (i = 0; i < buttontags.length; i++) {
+        buttontags[i].disabled = true;
+        buttontags[i].style.backgroundColor = "#DADAD9";
+    }
+}
 function computerPlay() {
     let choice;
-    let number = Math.floor(Math.random() * (4-1) + 1);
+    let number = Math.floor(Math.random() * (3) + 1);
     if (number === 1) choice = "rock";
     if (number === 2) choice = "paper";
     if (number === 3) choice = "scissors";
     return choice;
 }
-
-function playerSelection() {
-    let userChoice;
-    while ((userChoice !== "rock") || (userChoice !== "paper") || (userChoice !== "scissors")) {
-        userChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
-        if ((userChoice === "rock") || (userChoice === "paper") || (userChoice === "scissors")) break;
-    }
-    return userChoice;
-}
 function playRound(computer, user) {
     computer = computerPlay();
-    user = playerSelection();
+    user = userChoice;
 
     if ((user === 'rock') && (computer === 'scissors')) {
         res = `You win! ${user.charAt(0).toUpperCase() + user.slice(1)} beats ${computer.charAt(0).toUpperCase() + computer.slice(1)}`;
@@ -49,18 +48,35 @@ function playRound(computer, user) {
     else if (user === computer) {
         res = `Tie`;
     }
-    return res;
-}
-function game() {
-    let finalWinner = "";
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Round ${i}: ${playRound()}`);
+    if (userPoints === 5 && (userPoints > computerPoints)) {
+        winnerText = `Legendary Enemy Felled. You are the new Elden Lord.`
+        disableButtons();
+        
     }
-    console.log(`User: ${userPoints}`)
-    console.log(`Computer: ${computerPoints}`)
-    if (userPoints > computerPoints) finalWinner = "User wins!";
-    else if (userPoints < computerPoints) finalWinner = "Computer wins!";
-    else finalWinner = "Tie";
-    return finalWinner;
+    else if (computerPoints === 5 && (computerPoints)) {
+        winnerText = `You died.`
+        disableButtons();
+    }
+    else {
+        winnerText = "";
+    }
 }
-console.log(game());
+
+//UI
+const rockbutton = document.getElementById(`rock`);
+const paperbutton = document.getElementById(`paper`);
+const scissorsbutton = document.getElementById(`scissors`);
+const buttons = document.querySelectorAll(`button`);
+let buttontags = document.getElementsByTagName(`button`);
+const userpoints = document.querySelector(`#userpoints`);
+const computerpoints = document.querySelector(`#computerpoints`)
+const content = document.querySelector(`.container`);
+const p = document.createElement(`p`);
+buttons.forEach(element => element.addEventListener(`click`, function(e) {
+    userChoice = element.getAttribute(`id`);
+    playRound();
+    userpoints.textContent = `User points: ${userPoints}`;
+    computerpoints.textContent = `Computer points: ${computerPoints}`;
+    p.textContent = `${winnerText}`;
+    content.appendChild(p)
+}));
